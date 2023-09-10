89568dce5638b84e4aa80544cf1447519edb1cde
RostislavLegenda <143929913+RostislavLegenda@users.noreply.github.com>
2023-09-10 11:07:22 +0300

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,31 @@
+from pygame import *
+from random import randint
+from time import time as timer
+font.init()
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_W]:
+            self.rect.y -= self.speed
+        if keys[K_S]
+            self.rect.y -= self.speed
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost 
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
